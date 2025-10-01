191cdd9815098f001dc6200ad70f396aa15008ae
WilliamHHL <npmps987@gmail.com>
2025-10-01 21:57:08 +0800

    update the tb and WB

--- vsrc/tb_top.v
+++ vsrc/tb_top.v
@@ -6,7 +6,7 @@ module tb_top;
     reg rst = 1;
     wire [31:0] pc, instr, x1, x2, x3, x4;
 
-    // Connect your top module
+    // Connect top module
     top uut (
         .clk(clk),
         .rst(rst),
@@ -31,10 +31,21 @@ module tb_top;
     #20;
     rst = 0;
 
-    repeat (10) begin
-        @(negedge clk);
-        $display("PC=%08x | instr=%08x | x1=%08x x2=%08x x3=%08x x4=%08x", pc, instr, x1, x2, x3, x4);
-    end
+    repeat (16) begin
+        @(posedge clk);
+        #1;
+        $display("PC=%08x instr=%08x | rd=%0d wen=%0d wb=%08x | rs1=%0d rs2=%0d | x2=%08x x3=%08x x4=%08x",
+        pc, instr,
+        tb_top.uut.u_regfile.rd_addr,
+        tb_top.uut.u_regfile.rd_wen,
+        tb_top.uut.u_WB.wb_data,
+        tb_top.uut.u_ID.rs1_addr,
+        tb_top.uut.u_ID.rs2_addr,
+        tb_top.uut.u_regfile.regs[2],
+        tb_top.uut.u_regfile.regs[3],
+        tb_top.uut.u_regfile.regs[4]
+        );
+        end
 
     for (i = 0; i < 32; i = i + 1) begin
         $display("x%0d = %08x", i, tb_top.uut.u_regfile.regs[i]);
