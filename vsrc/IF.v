08ba697850f39b3aba7bc467bcdea2ff4adfe009
WilliamHHL <npmps987@gmail.com>
2025-10-01 21:52:33 +0800

    change the imem to pure comb rom

--- vsrc/IF.v
+++ vsrc/IF.v
@@ -1,10 +1,10 @@
 module IF (
-    input clk,
+    //input clk,
     input [31:0] pc,
     output  [31:0] instr
 );
     imem u_imem (
-        .clk(clk),
+        //.clk(clk),
         .addr(pc),
         .data(instr)
     );
--- vsrc/imem.v
+++ vsrc/imem.v
@@ -2,27 +2,17 @@
 // verilator lint_off UNDRIVEN
 
 module imem(
-    input        clk,
     input  [31:0] addr,
     output [31:0] data
 );
 
     reg [31:0] mem [0:255];
-    reg [7:0] addr_reg;
-
     initial begin
         $readmemh("program.hex", mem);
     end
-    reg [31:0] offset;
-    always @(posedge clk) begin
-        
-        if (addr >= 32'h8000_0000)
-            offset = (addr - 32'h8000_0000) >> 2;
-        else
-            offset = 0;
-        addr_reg <= offset[7:0];
-    end
 
-    assign data = mem[addr_reg];
+    wire [31:0]offset = (addr >= 32'h8000_0000)? (addr - 32'h8000_0000) >> 2:0;
+
+    assign data = mem[offset[7:0]];
 
 endmodule
